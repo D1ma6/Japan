c2aa49c4c36ef0402b1a1163b665f5bf2c070fb9
Dmytro Bula <64990670+D1ma6@users.noreply.github.com>
2020-05-27 16:06:00 +0100

    preloader animation added

--- script/App.js
+++ script/App.js
@@ -1,22 +1,26 @@
-window.addEventListener("load", () => {
-  const preload = document.querySelector(".preload");
-  preload.classList.add("preload-finish");
-});
-
 const imgContainer = document.querySelectorAll(".hero__right__container");
-const heroRight = document.querySelectorAll(".hero__right");
 
-console.log(imgContainer);
+// event listeners
+window.addEventListener("load", loadOn());
+
+function loadOn() {
+  setInterval(() => {
+    const preload = document.querySelector(".preload");
+    preload.classList.add("preload-finish");
+  }, 2000);
+}
 
 imgContainer.forEach((img) => {
   img.addEventListener("mouseover", () => {
     for (let i = 0; i < imgContainer.length; i++) {
       imgContainer[i].classList.add("hero__right__container-hover");
+      img.style.overflow = "visible";
     }
   });
   img.addEventListener("mouseleave", () => {
     for (let i = 0; i < imgContainer.length; i++) {
       imgContainer[i].classList.remove("hero__right__container-hover");
+      img.style.overflow = "hidden";
     }
   });
 });
